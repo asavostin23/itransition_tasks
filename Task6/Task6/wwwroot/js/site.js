fbdf0b1458f36c3162aa1f23d146908b458d9de0
Anton Savostin <asavostin23@gmail.com>
2022-08-02 15:44:39 +0300

    Бесконечная прокрутка

--- Task6/Task6/wwwroot/js/site.js
+++ Task6/Task6/wwwroot/js/site.js
@@ -13,4 +13,49 @@
         }
         document.querySelector('table tbody').innerHTML = htmlString;
     }
+    window.addEventListener("scroll", throttle(checkPosition, 350));
+    window.addEventListener("resize", throttle(checkPosition, 350));
 });
+
+async function checkPosition() {
+    const height = document.body.offsetHeight;
+    const screenHeight = window.innerHeight;
+    const scrolled = window.scrollY;
+    const threshold = height - screenHeight / 4;
+    const position = scrolled + screenHeight;
+    if (position >= threshold) {
+        await loadPage(nextPage++);
+    }
+}
+function throttle(callee, timeout) {
+    let timer = null;
+    return function perform(...args) {
+        if (timer) return
+        timer = setTimeout(() => {
+            callee(...args);
+            clearTimeout(timer);
+            timer = null;
+        }, timeout);
+    }
+}
+async function loadPage(page) {
+    if (isLoading) return;
+    isLoading = true;
+    let seed = document.querySelector('#seedInput').value;
+    let url = '/peoplegen/' + document.querySelector('#selectRegion').value + '?seed=' + seed + '&page=' + page;
+    let response = await fetch(url);
+    if (response.ok) {
+        let people = await response.json();
+        let htmlString = '';
+        let i = Number(document.querySelector('table tbody').lastChild.firstChild.innerHTML) + 1;
+        for (let person of people) {
+            htmlString += '<tr><td>' + (i++).toString() + '</td><td>' + person.uniqueId + '</td><td>' + person.surname + '</td><td>' + person.name + '</td><td>' + person.patronymic + '</td><td>' + person.adress + '</td><td>' + person.phone + '</td></tr>';
+        }
+        document.querySelector('table tbody').innerHTML += htmlString;
+    }
+    isLoading = false;
+}
+
+
+let nextPage = 1;
+let isLoading = false;
